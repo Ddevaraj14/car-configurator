17f1451c038555c60f78743b3ca8ce376a32aebd
Ddevaraj <ddevaraj@synamedia.com>
2024-02-20 00:31:28 +0530

    Added interaction to change material

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import Experience from "./components/Experience";
 import Configurator from "./components/Configurator";
+import { CustomizationProvider } from "./contexts/Customization";
 
 function App() {
-  return <div className="App">
-    <Canvas>
-    <color attach="background" args={['#191920']} />
-    <fog attach="fog" args={['#191920', 0, 15]} />
-    <Experience />
-    </Canvas>
-    <Configurator />
-  </div>;
-};
+  return (
+    <div className="App">
+      <CustomizationProvider>
+        <Canvas>
+          <color attach="background" args={["#191920"]} />
+          <fog attach="fog" args={["#191920", 0, 15]} />
+          <Experience />
+        </Canvas>
+        <Configurator />
+      </CustomizationProvider>
+    </div>
+  );
+}
 
 export default App;
--- src/components/Configurator.jsx
+++ src/components/Configurator.jsx
@@ -1,22 +1,29 @@
-import './Config.css';
+import { useCustomization } from "../contexts/Customization";
+import "./Config.css";
 
 const Configurator = () => {
+  const { material, setMaterial } = useCustomization();
+  console.log("material", material);
+
   return (
     <div className="configuration">
-        <div className="configurator_section">
-            <div className="configurator_section_title">
-                Chair Material
-            </div>
-            <div className="configurator_section_values">
-                <div className="item">
-                    <div className="item_label">Leather</div>
-                    <div className="item_label">Fabric</div>
-
-                </div>
-
-            </div>
-
+      <div className="configurator_section">
+        <div className="configurator_section_title">Chair Material</div>
+        <div className="configurator_section_values">
+          <div
+            className={'item ${material === "leather" ? "item--active": ""}'}
+            onClick={() => setMaterial("leather")}
+          >
+            <div className="item_label">Leather</div>
+          </div>
+          <div
+            className={'item ${material === "fabric" ? "item--active": ""}'}
+            onClick={() => setMaterial("fabric")}
+          >
+            <div className="item_label">Fabric</div>
+          </div>
         </div>
+      </div>
     </div>
   );
 };
