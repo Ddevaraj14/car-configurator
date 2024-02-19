dbbd20bbd56dadfcc6cf42438772b6b2893a3d5a
Ddevaraj <ddevaraj@synamedia.com>
2024-02-19 21:53:12 +0530

    Added 3D Model

---
+++ src/components/Configurator.jsx
@@ -0,0 +1,24 @@
+import './Config.css';
+
+const Configurator = () => {
+  return (
+    <div className="configuration">
+        <div className="configurator_section">
+            <div className="configurator_section_title">
+                Chair Material
+            </div>
+            <div className="configurator_section_values">
+                <div className="item">
+                    <div className="item_label">Leather</div>
+                    <div className="item_label">Fabric</div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+  );
+};
+
+export default Configurator;
---
+++ src/components/Experience.jsx
@@ -0,0 +1,37 @@
+import { MeshReflectorMaterial, PresentationControls, Stage } from "@react-three/drei";
+import { Suspense } from "react";
+import Chair from "./Chair";
+
+const Experience = () => {
+
+  return (
+    <PresentationControls  speed={1.5} global zoom={1} polar={[0.1, Math.PI / 4]} >
+
+    <Stage environment={'studio'} intensity={0.6} contactShadows={false} />
+    <Suspense fallback={null}>
+        <Chair />
+    </Suspense>
+    <Stage />
+
+    <mesh rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[170, 170]} />
+        <MeshReflectorMaterial
+          blur={[300, 100]}
+          resolution={2048}
+          mixBlur={1}
+          mixStrength={40}
+          roughness={1}
+          depthScale={1.2}
+          minDepthThreshold={0.4}
+          maxDepthThreshold={1.4}
+          color="#050505"
+          metalness={0.5}
+          />
+      </mesh>
+
+    </PresentationControls>
+  );
+};
+
+
+export default Experience;
